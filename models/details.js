3547099938abe3269d4652b955af1cb73db91c65
xander <shuaibualexander@gmail.com>
2021-06-25 15:40:28 +0100

    make it better

---
+++ models/details.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+// define an empty schema on order to get all the data from the collection
+const dataSchema = new mongoose.Schema({
+    voltage: Number,
+    current: Number,
+    temperature: Number,
+    batteryVoltage :Number
+},{
+    timestamps: { createdAt: '_created_at', updatedAt: '_updated_at' }
+});
+
+// define the data model based on an existing collection
+const dataModel = mongoose.model('details', dataSchema);
+
+module.exports = dataModel;
---
+++ routes/details.js
@@ -0,0 +1,33 @@
+var express = require('express');
+const detailsModel = require('../models/details');
+var router = express.Router();
+
+
+/* POST details listing. */
+router.post('/', async function(req, res) {
+  const {
+    voltage,
+    current,
+    temperature,
+    batteryVoltage
+  } = req.body;
+
+  // save the data to the model
+  const newDetail = new detailsModel({
+    voltage,
+    current,
+    temperature,
+    batteryVoltage
+  });
+  await newDetail.save()
+  res.send({data: newDetail});
+});
+
+/* GET all listings */
+router.get('/', async function(req, res) {
+  const allDetails = await detailsModel.find();
+  res.send({data: allDetails});
+})
+
+
+module.exports = router;
